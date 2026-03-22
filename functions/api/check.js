4a3e65ed428c246859dcf8af06877950b7fe117e
businessaicardstudio-debug <business.aicardstudio@gmail.com>
2026-03-22 20:01:05 +0530

    Create check.js

---
+++ functions/api/check.js
@@ -0,0 +1,7 @@
+export default {
+  async fetch(request) {
+    // ... same structure as kill but NO holds, just PM/token create tests
+    // (Full code below in kill, duplicate & remove kill loop)
+  }
+};
+
